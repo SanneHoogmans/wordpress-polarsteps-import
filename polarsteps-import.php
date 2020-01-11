bb655748ccaf76e8ec3d60e6392accab75db1ed8
Sanne Hoogmans <sanne@weprovide.com>
2020-01-11 16:36:06 +0100

    init module

---
+++ polarsteps-import.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: Polarsteps Import
+ * Description: Creates Wordpress Blog Posts from Polarsteps Export Tar file
+ * Version: 1.0
+ * Author: Sanne Hoogmans & Julian van den Berkmortel
+ **/
